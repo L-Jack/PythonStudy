8e403b29e37be9b4ed63f6e2bd4ea4aef9a11aa9
L-Jack <934713803@qq.com>
2018-08-11 16:34:42 +0800

    Initial commit

---
+++ .ipynb_checkpoints/test.py
@@ -0,0 +1,14 @@
+def stu(name, age):
+    '''
+    这是文档文字内容
+    :param name: 学生的名字
+    :param age: 学生的年龄
+    :return: 这个函数没有返回值
+    '''
+    pass
+
+print(help(stu))
+
+print("*" * 20)
+
+print(stu.__doc__)
